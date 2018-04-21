2b3e7c218ac7ac3b9a57491d7a47aa455b6f05a3
mer163 <mer163@sina.com>
2018-04-21 14:19:36 +0800

    修复启动和重启命令

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-nohup python3  manage.py  runserver --host 192.168.0.24 --port 20088 >>log.log 2>&1 &
+ip=`ifconfig|grep 192.168| awk -F ':' '{print $2}'|awk '{print $1}'`
+nohup python3  manage.py  runserver --host $ip --port 20088 >>log.log 2>&1 &

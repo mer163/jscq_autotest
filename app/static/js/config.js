5c14bccda0447966704deb5d5291e2c6a68216ee
mer163 <mer163@sina.com>
2018-04-23 16:53:33 +0800

    修改请求ip

--- app/static/js/config.js
+++ app/static/js/config.js
@@ -3,10 +3,5 @@
 // var mockUrl="http://localhost:5001/mock";
 
 
-// var myUrl="http://192.168.0.24:20088/testcases";
-// var mockUrl="http://192.168.0.24:20088/mock";
-
-var urlAndPort=window.location.protocol + "//"+ window.location.host + ":" +window.location.port
-
-var myUrl=urlAndPort + "/testcases"
-var mockUrl= urlAndPort +"/mock"
+var myUrl="http://192.168.0.24:20088/testcases";
+var mockUrl="http://192.168.0.24:20088/mock";

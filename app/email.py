88c7355b172821f9984ee9f3bdad334b6613892c
mer163 <mer163@sina.com>
2018-04-27 21:05:25 +0800

    添加email日志

--- app/email.py
+++ app/email.py
@@ -3,7 +3,12 @@ from flask import current_app, render_template
 from flask_mail import Message
 import threading
 from . import mail
+import logging
 
+logging.basicConfig(filename='monitor.log',format='%(asctime)s -%(name)s-%(levelname)s-%(module)s:%(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S %p',
+                    level=logging.DEBUG)
+logger = logging.getLogger('manage')
 
 def send_async_email(app, msg):
     with app.app_context():
@@ -14,15 +19,20 @@ def send_email(to, subject, template, **kwargs):
     app = current_app._get_current_object()
     msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
                   sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
-    print("主题：%s", app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject)
-    print("sender:%s",app.config['FLASKY_MAIL_SENDER'])
-    print("recivier:%s",[to])
+    logger.info("主题：%s", app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject)
+    logger.info("sender:%s",app.config['FLASKY_MAIL_SENDER'])
+    logger.info("recivier:%s",[to])
+    logger.info(msg)
+
+    # print("主题：%s", app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject)
+    # print("sender:%s",app.config['FLASKY_MAIL_SENDER'])
+    # print("recivier:%s",[to])
 
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
-    print(msg)
-    print(msg.body)
-    print(msg.html)
+    # print(msg)
+    # print(msg.body)
+    # print(msg.html)
 
 
     thr = threading.Thread(target=send_async_email, args=[app, msg])

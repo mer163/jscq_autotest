23e81026c8e1b5539b81e7dca381da63f79d38b4
mer163 <mer163@sina.com>
2018-09-13 18:44:31 +0800

    modify restart.sh

--- restart.sh
+++ restart.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #从进程中找到运行的程序，并杀掉
-count=`ps aux |grep 'python3 manage.py runserver' |awk 'END{print NR}'`
+count=`ps aux |grep 'python3 manage.py runserver' |grep -v grep |awk 'END{print NR}'`
 
 if [[ $count -gt 1 ]]; then
   echo '发现flask服务已启动，启动kill杀掉服务'
-  kill -9 `ps aux |grep python3 | grep manage|awk '{print $2}'`
+  kill -9 `ps aux |grep 'python3 manage.py runserver' | grep manage |grep -v grep |awk '{print $2}'`
 else
   echo 'flask服务未启动，准备启动服务'
 #重启服务
@@ -15,11 +15,11 @@ fi
 echo 'flask服务启动成功'
 
 
-coreservicecount=`ps aux |grep 'coreservice' |awk 'END{print NR}'`
+coreservicecount=`ps aux |grep 'coreservice' |grep -v grep |awk 'END{print NR}'`
 
 if [[ $coreservicecount -gt 1 ]]; then
   echo '发现coreservice服务已启动，启动kill杀掉服务'
-  kill -9 `ps aux |grep coreservice|awk '{print $2}'`
+  kill -9 `ps aux |grep coreservice |grep -v grep |awk '{print $2}'`
 else
   echo '服务未启动，准备启动服务'
 #重启服务

4451e0adb8a1924b14c61c431d4c73b5d99cc698
mer163 <mer163@sina.com>
2018-09-13 18:40:33 +0800

    update .gitignore

--- restart.sh
+++ restart.sh
@@ -4,12 +4,25 @@
 count=`ps aux |grep 'python3 manage.py runserver' |awk 'END{print NR}'`
 
 if [[ $count -gt 1 ]]; then
-  echo '发现服务已启动，启动kill杀掉服务'
+  echo '发现flask服务已启动，启动kill杀掉服务'
   kill -9 `ps aux |grep python3 | grep manage|awk '{print $2}'`
 else
-  echo '服务未启动，准备启动服务'
+  echo 'flask服务未启动，准备启动服务'
 #重启服务
 fi
 
 ./start.sh
-echo '服务启动成功'
+echo 'flask服务启动成功'
+
+
+coreservicecount=`ps aux |grep 'coreservice' |awk 'END{print NR}'`
+
+if [[ $coreservicecount -gt 1 ]]; then
+  echo '发现coreservice服务已启动，启动kill杀掉服务'
+  kill -9 `ps aux |grep coreservice|awk '{print $2}'`
+else
+  echo '服务未启动，准备启动服务'
+#重启服务
+fi
+./start_coreservice.sh
+echo 'coreservice服务启动成功'

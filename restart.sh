03627a45f24d53c7ec5e67bb378dac4bfd9f34f6
mer163 <mer163@sina.com>
2018-09-13 18:47:12 +0800

    modify restart.sh

--- restart.sh
+++ restart.sh
@@ -6,6 +6,7 @@ count=`ps aux |grep 'python3 manage.py runserver' |grep -v grep |awk 'END{print 
 if [[ $count -gt 1 ]]; then
   echo '发现flask服务已启动，启动kill杀掉服务'
   kill -9 `ps aux |grep 'python3 manage.py runserver' | grep manage |grep -v grep |awk '{print $2}'`
+  echo 'kill successful'
 else
   echo 'flask服务未启动，准备启动服务'
 #重启服务
@@ -20,6 +21,7 @@ coreservicecount=`ps aux |grep 'coreservice' |grep -v grep |awk 'END{print NR}'`
 if [[ $coreservicecount -gt 1 ]]; then
   echo '发现coreservice服务已启动，启动kill杀掉服务'
   kill -9 `ps aux |grep coreservice |grep -v grep |awk '{print $2}'`
+  echo 'kill seccessfull'
 else
   echo '服务未启动，准备启动服务'
 #重启服务

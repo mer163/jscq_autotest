474b48cde83ac79b838ea7bfaf222fbf27bfa5b6
mer163 <mer163@sina.com>
2018-10-25 12:58:16 +0800

    修改utils.js

--- app/static/js/utils.js
+++ app/static/js/utils.js
@@ -1,5 +1,5 @@
 //获取本地gitlab地址
 function getGitlabhost() {
 	console.log("http://"+self.location.hostname+":20090")
-	self.location.href=self.location.hostname+":20090"
+	self.location.href="http://"+self.location.hostname+":20090"
 }

df7073eebfb22216a9e8ac0ad680f7f8d2eda6f7
mer163 <mer163@sina.com>
2018-11-26 15:42:23 +0800

    modify start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #获取服务器本地ip地址
-ip=`ifconfig|grep 192.168| awk -F ':' '{print $2}'|awk '{print $1}'`
+#ip=`ifconfig|grep 192.168| awk -F ':' '{print $2}'|awk '{print $1}'`
 
 #启动服务，日志保存至log.log文件中
 nohup python3  manage.py  runserver --host 0.0.0.0 --port 20088 >app/log/serverlog.log 2>&1 &
